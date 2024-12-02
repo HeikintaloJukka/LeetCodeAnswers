19afc38adbf975726b620c05b1769af7261e2f82
HeikintaloJukka <heikintalo.jukka@gmail.com>
2024-12-02 20:27:46 +0200

    Time: 0 ms (100.00%) | Memory: 51.2 MB (48.78%) - LeetSync

---
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.ts
@@ -0,0 +1,14 @@
+function isPrefixOfWord(sentence: string, searchWord: string): number {
+    let words: string[] = sentence.split(" ")
+    for(let i=0;i<words.length;i++){
+        if(words[i].substring(0,searchWord.length) === searchWord){
+            return i+1
+        }
+    }
+
+    return -1
+};
+
+function prefixCheck(word: string){
+    
+}

bf2db25fac594d27de301468d315f7755d433b83
HeikintaloJukka <heikintalo.jukka@gmail.com>
2024-12-28 23:06:28 +0200

    Time: 1336 ms (5.05%) | Memory: 58.6 MB (22.48%) - LeetSync

---
+++ 39-combination-sum/combination-sum.ts
@@ -0,0 +1,29 @@
+function combinationSum(candidates: number[], target: number): number[][] {
+    let ans = []
+    let backtrack = (combined, curCombArr) => {
+
+        if(combined % target === 0 && combined > 0){
+            let temp = curCombArr.slice("").sort()
+
+            //only unique num combs
+            for(let prev of ans){
+                if(ans.length > 0 && JSON.stringify(temp) === JSON.stringify(prev) ){ 
+                    return
+                }
+            }
+
+            ans.push(temp)
+            return
+        }
+
+        for(let cand of candidates){
+            if(combined + cand <= target){
+                curCombArr.push(cand)
+                backtrack(combined + cand,curCombArr)
+                curCombArr.pop()
+            }
+        }
+    }
+    backtrack(0,[])
+    return ans
+};

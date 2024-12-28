a95d2935218d0455287d1638425a652588940a3f
HeikintaloJukka <heikintalo.jukka@gmail.com>
2024-12-28 23:24:16 +0200

    Time: 38 ms (9.86%) | Memory: 58.9 MB (19.47%) - LeetSync

--- 39-combination-sum/combination-sum.ts
+++ 39-combination-sum/combination-sum.ts
@@ -1,6 +1,28 @@
 function combinationSum(candidates: number[], target: number): number[][] {
     let ans = []
-    let backtrack = (combined, curCombArr) => {
+    /*
+        const bt = (sum: number, idx: number, trace: number[]) => {
+        if (idx >= candidates.length || sum > target) {
+            return;
+        }
+
+        if (sum === target) {
+            result.push([...trace])
+            return;
+        }
+
+        trace.push(candidates[idx]);
+        bt(sum + candidates[idx], idx, trace);
+        trace.pop();
+        bt(sum, idx + 1, trace);
+
+    }
+
+    */
+    let backtrack = (combined, idx,curCombArr) => {
+        if (idx >= candidates.length || combined > target) {
+            return
+        }
 
         if(combined % target === 0 && combined > 0){
             let temp = curCombArr.slice("").sort()
@@ -16,14 +38,11 @@ function combinationSum(candidates: number[], target: number): number[][] {
             return
         }
 
-        for(let cand of candidates){
-            if(combined + cand <= target){
-                curCombArr.push(cand)
-                backtrack(combined + cand,curCombArr)
-                curCombArr.pop()
-            }
-        }
+        curCombArr.push(candidates[idx])
+        backtrack(combined + candidates[idx],idx,curCombArr)
+        curCombArr.pop()
+        backtrack(combined,idx+1,curCombArr)
     }
-    backtrack(0,[])
+    backtrack(0,0,[])
     return ans
 };

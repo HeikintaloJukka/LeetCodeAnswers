113bf2b0e5c047b17bc0ef1d01edd790ca66524f
HeikintaloJukka <heikintalo.jukka@gmail.com>
2025-01-01 20:02:14 +0200

    Time: 1 ms (21.34%) | Memory: 51.4 MB (79.98%) - LeetSync

--- 27-remove-element/remove-element.ts
+++ 27-remove-element/remove-element.ts
@@ -1,28 +1,11 @@
 function removeElement(nums: number[], val: number): number {
-    let count=0
-    for(let i=0,k=0;i<nums.length;i++,k++){
-        if(nums[i]===val){
-            count++
-            let length = nums.length
-            for(let j=1;j<length;j++){
-                if(nums[k+j] !== undefined && nums[k+j] !== val){
-                    nums[i]=nums[k+j]
-                    nums[k+j]=undefined
-                    k++
-                    break
-                }
-            }
-        }else if(nums[i] === undefined){
-            let length = nums.length
-            for(let j=1;j<length;j++){
-                if(nums[i+j] !== undefined && nums[i+j] !== val){
-                    console.log("wot")
-                    nums[i]=nums[i+j]
-                    nums[i+j]=undefined
-                    break
-                }
-            }
+
+    let i=0
+    for(let j=0;j<nums.length;j++){
+        if(nums[j] != val){
+            nums[i] = nums[j]
+            i++
         }
     }
-    return nums.length - count
+    return i
 };

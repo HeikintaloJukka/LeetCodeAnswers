76eff12d1bd3ae02ded5924a6df50994811c66be
HeikintaloJukka <heikintalo.jukka@gmail.com>
2025-01-08 16:46:40 +0200

    Time: 5 ms (31.43%) | Memory: 51.5 MB (97.14%) - LeetSync

---
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.ts
@@ -0,0 +1,21 @@
+function countPrefixSuffixPairs(words: string[]): number {
+
+    let isPrefixAndSuffix = (str1:string,str2:string):boolean => {
+        let prefix = str2.slice(0,str1.length)
+        let suffix = str2.slice(str2.length-str1.length)
+        if(prefix === str1 && suffix === str1){
+            return true
+        }
+        return false
+    }
+    
+    let count = 0
+    for(let i=0;i<words.length-1;i++){
+        for(let j=i+1;j<words.length;j++){
+            if(isPrefixAndSuffix(words[i],words[j])){
+                count++
+            }
+        }
+    }
+    return count
+};

14677f8100969d508fd82c62ea2eab78c5da51be
HeikintaloJukka <heikintalo.jukka@gmail.com>
2025-01-20 16:24:09 +0200

    Time: 2 ms (87.25%) | Memory: 54.5 MB (89.26%) - LeetSync

--- 47-permutations-ii/permutations-ii.ts
+++ 47-permutations-ii/permutations-ii.ts
@@ -1,28 +1,55 @@
-function permuteUnique(nums: number[]): number[][] {
-    let res = []
-    let counter: { [key: number]: number } = {};
-    for (let num of nums) {
-        if (!(num in counter)) counter[num] = 0;
-        counter[num]++;
-    }
+/*function permuteUnique(nums: number[]): number[][] {
+    const results: number[][] = [];
+    const visited: boolean[] = new Array(nums.length).fill(false);
+    nums.sort((a, b) => a - b);
 
-    let backtrack = (path,n) => {
-        if(path.length === n){
-            res.push([...path])
+    const backtrack = (path: number[]) => {
+        if (path.length === nums.length) {
+            results.push([...path]);
+            return;
+        }
+
+        for (let i = 0; i < nums.length; i++) {
+            if (visited[i]) continue;
+            if (i > 0 && nums[i] === nums[i - 1] && !visited[i - 1]) continue;
+
+            visited[i] = true;
+            path.push(nums[i]);
+
+            backtrack(path);
+
+            path.pop();
+            visited[i] = false;
+        }
+    };
+
+    backtrack([]);
+    return results;
+
+};*/
+
+function permuteUnique(nums: number[]): number[][] {
+    let combs = []
+    let visited = new Array(nums.length).fill(false)
+    nums.sort((a,b) => a-b)
+    
+    let backtrack = (path) => {
+        if(path.length === nums.length){
+            combs.push([...path])
             return
         }
 
-        for(let num in counter){
-            if(counter[num] == 0) continue
-            path.push(parseInt(num))
-            counter[num]--
-            backtrack(path, n)
+        for(let i=0;i<nums.length;i++){
+            if(visited[i]) continue
+            if (i > 0 && nums[i] === nums[i - 1] && !visited[i - 1]) continue;
+            visited[i] = true
+            path.push(nums[i])
+            backtrack(path)
             path.pop()
-            counter[num]++
+            visited[i] = false;
         }
-
     }
-    backtrack([],nums.length)
+    backtrack([])
 
-    return res
+    return combs
 };

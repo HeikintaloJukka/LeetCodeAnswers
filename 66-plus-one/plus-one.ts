dbf8e27f11aa5d70ace5b10deab149b7e687e655
HeikintaloJukka <heikintalo.jukka@gmail.com>
2025-01-26 21:08:19 +0200

    Time: 8 ms (2.73%) | Memory: 51.9 MB (5.66%) - LeetSync

---
+++ 66-plus-one/plus-one.ts
@@ -0,0 +1,20 @@
+function plusOne(digits: number[]): number[] {
+    console.log(digits)
+    for(let i=digits.length-1;i>=0;i--){
+        let num = digits[i] +1
+        if(num % 10 === 0){
+            digits[i] = 0
+        }
+        else{
+            digits[i] = num
+            return digits
+        }
+
+        //out of nums to handle +1 remaining
+        if(i===0){
+            let temp = "1,"+digits
+            return temp.split(",").map(Number);
+        }
+    }
+    return []
+};

85381f8dbdb337280dc08135cad5a5e6e982c059
HeikintaloJukka <heikintalo.jukka@gmail.com>
2025-02-22 23:06:42 +0200

    Time: 43 ms (20.64%) | Memory: 65.7 MB (33.26%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.ts
+++ 41-first-missing-positive/first-missing-positive.ts
@@ -1,9 +1,6 @@
 function firstMissingPositive(nums: number[]): number {
     nums.sort((a,b) => {return a-b})
-    if(nums.length === 1){
-        if(nums[0] !== 1) return 1
-        else return 2
-    }
+
 
     let negMod = 0
     for(let i=0;i<nums.length+1;i++){

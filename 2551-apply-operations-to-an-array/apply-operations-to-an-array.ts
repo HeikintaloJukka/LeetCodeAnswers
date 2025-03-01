97658eeb2ffcb03820787ee3ce6326beeed3677c
HeikintaloJukka <heikintalo.jukka@gmail.com>
2025-03-01 21:58:01 +0200

    Time: 2 ms (62.75%) | Memory: 58.8 MB (25.49%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.ts
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.ts
@@ -1,25 +1,27 @@
 function applyOperations(nums: number[]): number[] {
+    let zeroCount = 0
+    let newArr = []
     for(let i=0;i<nums.length-1;i++){
         if(nums[i] === nums[i+1]){
             nums[i] = nums[i]*2
             nums[i+1] = 0
         }
+
+        if(nums[i] !== 0){
+            newArr.push(nums[i])
+
+        }
+        else zeroCount++
     }
 
-    let zeroCount = 0
-    let newArr = []
-    for(let i=0;i<nums.length;i++){
-        if(nums[i] === 0){
-            zeroCount++
-        }
-        else newArr.push(nums[i])
+    if(nums[nums.length-1] !== 0){
+        newArr.push(nums[nums.length-1])
+    }
+    else zeroCount++
 
-        if(i === nums.length-1){
-            while(zeroCount > 0){
-                zeroCount--
-                newArr.push(0)
-            }
-        }
+    while(zeroCount > 0){
+        zeroCount--
+        newArr.push(0)
     }
 
     return newArr

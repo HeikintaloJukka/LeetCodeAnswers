162bab0854c65a4ff9dba7b3c300c54ef3b68c8f
HeikintaloJukka <heikintalo.jukka@gmail.com>
2025-03-17 18:26:02 +0200

    Time: 2 ms (83.87%) | Memory: 58.7 MB (9.68%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.ts
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.ts
@@ -2,15 +2,10 @@ function divideArray(nums: number[]): boolean {
     let map = new Map()
     for(let num of nums){
         if(map.has(num)){
-            map.set(num,map.get(num)+1)
+            map.delete(num)
         }
         else map.set(num, 1)
     }
 
-    for(let val of map){
-        if(!(val[1] % 2 === 0)){
-            return false
-        }
-    }
-    return true
+    return map.size === 0
 };

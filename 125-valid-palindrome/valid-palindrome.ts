3bb6e940d9b1aa3d9faecfa069cbd6826c846231
HeikintaloJukka <heikintalo.jukka@gmail.com>
2025-03-31 21:24:18 +0300

    Time: 5 ms (61.36%) | Memory: 58.4 MB (55.77%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.ts
+++ 125-valid-palindrome/valid-palindrome.ts
@@ -2,10 +2,14 @@ function isPalindrome(s: string): boolean {
     s = s.replace(/[^a-zA-Z0-9]/g, '')
     s = s.toLowerCase()
 
-    for(let i=0;i<s.length;i++){
-        if(s[i] !== s[s.length-1-i]){
+    let i=0
+    let j=s.length-1
+    while(i<j){
+        if(s[i] !== s[j]){
             return false
         }
+        i++
+        j--
     }
     return true
 };

9208e56bbea9d5ffbc25662047e1c37e75dbf345
HeikintaloJukka <heikintalo.jukka@gmail.com>
2025-04-10 17:19:33 +0300

    Time: 0 ms (100.00%) | Memory: 55.3 MB (69.62%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function preorderTraversal(root: TreeNode | null): number[] {
+    let ordered = []
+    let checkNode = (node) => {
+        if(!node || node.val === null) return
+
+        ordered.push(node.val)
+        checkNode(node.left)
+        checkNode(node.right)
+    }
+    checkNode(root)
+    return ordered
+};

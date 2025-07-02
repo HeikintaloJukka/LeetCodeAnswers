cc80383a53f8ef12b57fc4b9533f071ad98a0a4f
HeikintaloJukka <heikintalo.jukka@gmail.com>
2025-07-02 09:31:05 +0300

    Time: 21 ms (39.41%) | Memory: 77.7 MB (18.26%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.ts
@@ -0,0 +1,10 @@
+function containsDuplicate(nums: number[]): boolean {
+    let map = new Map()
+    for(let i=0;i<nums.length;i++){
+        if(map.has(nums[i])){
+            return true
+        }
+        else map.set(nums[i], 1)
+    }
+    return false
+};
